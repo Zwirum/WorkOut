c832500f2c45fbf771995409f1bc3bc44250b039
Zvirum <nastena5dm@mail.ru>
2020-07-03 17:48:55 +0300

    added main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-  
+  $('.gallery__slider').slick({
+    infinite:false,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    focusOnSelect: true,
+  }); 
 });
