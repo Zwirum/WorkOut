2bde280a6a5073a611d13f1d115f3682d287bf63
Zvirum <nastena5dm@mail.ru>
2020-07-06 20:05:06 +0300

    added contacts/rewievs/services pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,26 @@
 $(function(){
-  $('.gallery__slider').slick({
-    infinite:false,
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    focusOnSelect: true,
-  }); 
+  $('input[type="file"]').styler();
+  
+  $("#rateYo").rateYo({
+    rating: 5,
+    starWidth: "45px",
+    ratedFill: "#c11e3c",
+    fullStar: true,
+    spacing: "5px",
+  });
+  $("#rateYo-rew").rateYo({
+    rating: 4,
+    starWidth: "45px",
+    ratedFill: "#c11e3c",
+    spacing: "5px",
+    readOnly: true
+  });
+  $('.service-wrapper .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.service-wrapper').find('.tab-item').removeClass('active-tab').hide();
+      $('.service-wrapper .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
 });
